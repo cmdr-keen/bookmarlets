6ea0fcdc2711f11626424567fa460245f7fcfefe
Philip Braun <50513374+cmdr-keen@users.noreply.github.com>
2025-01-19 15:08:02 +0100

    repo start + add archive.ph lookup

---
+++ archive-ph-lookup.js
@@ -0,0 +1,13 @@
+window.location.replace("https://archive.ph/" + window.location.href);
+
+/*
+=== Alternative TLDs ===
+archive.ph
+archive.today
+archive.fo
+archive.is
+archive.li
+archive.md
+archive.vn
+archiveiya74codqgiixo33q62qlrqtkgmcitqx5u2oeqnmn5bpcbiyd.onion
+*/
